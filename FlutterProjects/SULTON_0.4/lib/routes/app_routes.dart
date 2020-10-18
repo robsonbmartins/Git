216f447f63d8677a327d93f4c860223b8251094d
João D Junior <joao.junior.11411@gmail.com>
2020-10-18 13:40:57 -0300

    Mudança de roteamento

--- FlutterProjects/SULTON_0.4/lib/routes/app_routes.dart
+++ FlutterProjects/SULTON_0.4/lib/routes/app_routes.dart
@@ -1,6 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/login.dart';
+import 'package:flutter_crud/views/ca_form.dart';
+import 'package:flutter_crud/views/ca_list.dart';
+import 'package:flutter_crud/views/ca_login.dart';
+import 'package:flutter_crud/views/log_list.dart';
+
 class AppRoutes {
-  static const HOME = '/';
-  static const CA_FORM = '/ca-form';
-  static const CA_LOGS = '/ca-logs';
-  static const CA_LOGIN = '/ca-login';
+  static const HOME = 'home';
+  static const CA_FORM = 'ca-form';
+  static const CA_LOGS = 'ca-logs';
+  static const CA_LOGIN = '/';
+}
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  // Here we'll handle all the routing
+  var args = settings.arguments;
+
+  switch (settings.name) {
+    case AppRoutes.HOME:
+      return MaterialPageRoute(
+          builder: (context) => CAList(
+                auth: new Auth(),
+                loggedUser: args,
+              ));
+    case AppRoutes.CA_LOGIN:
+      return MaterialPageRoute(builder: (context) => CALogin(auth: new Auth()));
+    case AppRoutes.CA_FORM:
+      return MaterialPageRoute(
+          builder: (context) => CAForm(
+                ca: args,
+              ));
+    case AppRoutes.CA_LOGS:
+      return MaterialPageRoute(
+          builder: (context) => CALogs(
+                ca: args,
+              ));
+    default:
+      return MaterialPageRoute(
+          builder: (context) => CAList(
+                auth: new Auth(),
+              ));
+  }
 }
